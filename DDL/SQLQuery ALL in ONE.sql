5b2964733b5c4cf2c917f824891454a41ec42736
Evgeniy Pronyakin <ypronyakin@internet.ru>
2026-01-06 18:46:11 +0300

    Добавлены вспомогательные таблицы связей

--- DDL/SQLQuery ALL in ONE.sql
+++ DDL/SQLQuery ALL in ONE.sql
@@ -125,3 +125,43 @@ PRIMARY KEY(student, discipline)
 PRINT '					таблица "Exams" создана';
 
 PRINT 'Создание вспомогательных таблиц:';
+--PJT - Pure Joint Table
+CREATE TABLE TeacherDisciplinRelation
+(
+teacher				INT,
+discipline			SMALLINT,
+PRIMARY KEY(teacher, discipline),
+CONSTRAINT FK_TDR_Teacher		FOREIGN KEY(teacher)	REFERENCES Teachers(teacher_id),
+CONSTRAINT FK_TDR_Discipline	FOREIGN KEY(discipline)	REFERENCES Disciplines(discipline_id)
+);
+PRINT '					связь "TeacherDisciplinRelation" создана';
+
+CREATE TABLE DisciplinesDirectionRelation
+(
+discipline			SMALLINT,
+direction			TINYINT,
+PRIMARY KEY(discipline,direction),
+CONSTRAINT FK_DDR_Discipline	FOREIGN KEY(discipline)	REFERENCES Disciplines(discipline_id),
+CONSTRAINT FK_DDR_Direction		FOREIGN KEY(direction)	REFERENCES Directions(direction_id)
+);
+PRINT '					связь "DisciplinesDirectionRelation" создана';
+
+CREATE TABLE RequiredDisciplines
+(
+discipline			SMALLINT,
+required_discipline	SMALLINT,
+PRIMARY KEY(discipline,required_discipline),
+CONSTRAINT FK_RD_Discipline		FOREIGN KEY(discipline)	REFERENCES	Disciplines(discipline_id),
+CONSTRAINT FK_RD_Requires		FOREIGN KEY(required_discipline) REFERENCES Disciplines(discipline_id)
+);
+PRINT '					связь "RequiredDisciplines" создана';
+
+CREATE TABLE DependentDisciplines
+(
+discipline			SMALLINT,
+dependent_discipline SMALLINT,
+PRIMARY KEY(discipline,dependent_discipline),
+CONSTRAINT FK_DD_Discipline		FOREIGN KEY(discipline)	REFERENCES Disciplines(discipline_id),
+CONSTRAINT FK_DD_Dependent		FOREIGN KEY(dependent_discipline) REFERENCES Disciplines(discipline_id)
+);
+PRINT '					связь "DependentDisciplines" создана';
